1092690cf621113de41065d8c23e6be45037ebac
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-15 08:51:27 +0700

    Update product routes to render EJS views from the correct directory structure; added API endpoint for products.

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const productRouter = Router();
 
-productRouter.get("products", (req, res) => res.render("product.ejs"));
+productRouter.get("products", (req, res) => res.render("product/index.ejs"));
+
+productRouter.get("api/products", (req, res) => res.render("product.ejs"));
 
 export default productRouter;
