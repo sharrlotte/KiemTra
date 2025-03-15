063cc7dd209562d89a6d250c3d12650a45708669
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-15 07:56:09 +0700

    Enhance server setup by adding body-parser middleware, view engine configuration, and static file serving. Reorganize dotenv configuration for clarity.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
-
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const bodyParser = require("body-parser");
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/public"));
+
+const controller = require(__dirname + "/apps/controllers");
+app.use(controller);
+dotenv.config();
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
