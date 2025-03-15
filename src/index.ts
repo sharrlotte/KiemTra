bff1e61446f7ef14e92a9f2028ee0f4ceb85985a
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-15 08:21:56 +0700

    Add EJS as a view engine and update dependencies in package.json; refactor view directory structure in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import router from "./controller";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -9,11 +10,13 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set("views", __dirname + "/apps/views");
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use("/static", express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 dotenv.config();
+
+app.use(router);
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
